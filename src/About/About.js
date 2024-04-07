268d96a7e75c3ba0704405bfdc510bf6cd3ed5f6
raffa-raffa <rafaelacnh@gmail.com>
2024-04-07 15:31:11 -0300

    chore: :wrench: configuring routing with react-router-dom

---
+++ src/About/About.js
@@ -0,0 +1,11 @@
+import React from "react"
+import styles from "./About.module.css"
+
+const About = () => {
+
+    return(
+    <div>
+        <h1 className={styles}>About</h1>
+    </div>)}
+
+export default About
--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Home/Home';
+import About from './About/About';
 
 function App() {
   return (
     <div className="App">
-     <h1>MyBlog</h1>
+      <BrowserRouter>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+        </Routes>
+      </div>
+      </BrowserRouter>
     </div>
   );
 }
