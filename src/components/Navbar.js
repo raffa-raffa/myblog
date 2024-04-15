48cca4c0f36c85fe2544c0ce96258fa44cb2f3c8
raffa-raffa <rafaelacnh@gmail.com>
2024-04-14 21:08:23 -0300

    chore: :hammer: link para user logado

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,41 +1,78 @@
 import { NavLink } from "react-router-dom";
 
 
+import { useAuthValue } from "../context/AuthContext";
+
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
-    return (
-        <nav className={styles.navbar}>
-            <NavLink className={styles.brand} to="/">
-                Mini <span>Blog</span>
-            </NavLink>
-            <ul className={styles.links_list}>
-                <li>
-                    <NavLink
-                        to="/"
-                        className={({ isActive }) => (isActive ? styles.active : "")}
-                    >
-                        Home
-                    </NavLink>
-                </li>
-                        <li>
-                            <NavLink
-                                to="/register"
-                                className={({ isActive }) => (isActive ? styles.active : "")}
-                            >
-                                Cadastrar
-                            </NavLink>
-                        </li>
-                <li>
-                    <NavLink
-                        to="/about"
-                        className={({ isActive }) => (isActive ? styles.active : "")}
-                    >
-                        Sobre
-                    </NavLink>
-                </li>
-            </ul>
-        </nav>
-    )}
+  const { user } = useAuthValue();
+
+  return (
+    <nav className={styles.navbar}>
+      <NavLink className={styles.brand} to="/">
+        Mini <span>Blog</span>
+      </NavLink>
+      <ul className={styles.links_list}>
+        <li>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            Home
+          </NavLink>
+        </li>
+        {!user && (
+          <>
+            <li>
+              <NavLink
+                to="/login"
+                className={({ isActive }) => (isActive ? styles.active : "")}
+              >
+                Entrar
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/register"
+                className={({ isActive }) => (isActive ? styles.active : "")}
+              >
+                Cadastrar
+              </NavLink>
+            </li>
+          </>
+        )}
+        {user && (
+          <>
+            <li>
+              <NavLink
+                to="/posts/create"
+                className={({ isActive }) => (isActive ? styles.active : "")}
+              >
+                Novo post
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/dashboard"
+                className={({ isActive }) => (isActive ? styles.active : "")}
+              >
+                Dashboard
+              </NavLink>
+            </li>
+          </>
+        )}
+        <li>
+          <NavLink
+            to="/about"
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            Sobre
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Navbar;
