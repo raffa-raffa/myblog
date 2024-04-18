aada65ff8ec229e25cf019fdeb3787d86ac9b43c
raffa-raffa <rafaelacnh@gmail.com>
2024-04-18 02:52:27 -0300

    chore(wip): setando valores dos inputs do createPost

--- src/pages/CreatePost/CreatePost.js
+++ src/pages/CreatePost/CreatePost.js
@@ -1,12 +1,21 @@
-import styles from "./CreatePost.module.css";
+import { useState } from "react";
 
 const CreatePost = () => {
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
+  const [body, setBody] = useState("");
+  const [tags, setTags] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Título:", "URL da imagem:", image,title,"Conteúdo:", body, "Tags:", tags);
+  };
 
   return (
-    <div className={styles.create_post}>
+    <div>
       <h2>Criar post</h2>
       <p>Escreva sobre o que quiser e compartilhe o seu conhecimento!</p>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           <span>Título:</span>
           <input
@@ -14,6 +23,8 @@ const CreatePost = () => {
             name="text"
             required
             placeholder="Pense num bom título..."
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
           />
         </label>
         <label>
@@ -23,6 +34,8 @@ const CreatePost = () => {
             name="image"
             required
             placeholder="Insira uma imagem que representa seu post"
+            onChange={(e) => setImage(e.target.value)}
+            value={image}
           />
         </label>
         <label>
@@ -31,6 +44,8 @@ const CreatePost = () => {
             name="body"
             required
             placeholder="Insira o conteúdo do post"
+            onChange={(e) => setBody(e.target.value)}
+            value={body}
           ></textarea>
         </label>
         <label>
@@ -40,11 +55,11 @@ const CreatePost = () => {
             name="tags"
             required
             placeholder="Insira as tags separadas por vírgula"
+            onChange={(e) => setTags(e.target.value)}
+            value={tags}
           />
         </label>
-          <button className="btn" disabled>
-            Aguarde.. .
-          </button>
+        <button className="btn">Criar post!</button>
       </form>
     </div>
   );
