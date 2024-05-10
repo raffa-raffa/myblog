9915538bb77e2d8bbf6579c76afd343a181766d1
raffa-raffa <rafaelacnh@gmail.com>
2024-05-09 23:56:59 -0300

    chore: :hammer: exibindo 9 cards de noticias

--- src/components/NewsSection.js
+++ src/components/NewsSection.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import styles from "./News.module.css";
+
 
 const NewsSection = () => {
   const [news, setNews] = useState([]);
@@ -13,28 +15,30 @@ const NewsSection = () => {
             apiKey: process.env.REACT_NEWS_API_KEY
           }
         });
-        setNews(response.data.articles);
+        setNews(response.data.articles.slice(0, 9)); // Pegando apenas as primeiras 12 notícias
       } catch (error) {
         console.error('Erro ao buscar notícias:', error);
       }
     };
-
+  
     fetchNews();
-    console.log(news,'news')
   }, []);
+  
 
   return (
-    <div>
+    <div >
       <h2>Últimas Notícias do Brasil</h2>
       <ul>
-        {news.map((article, index) => (
-          <li key={index}>
-            {article.urlToImage && <img src={article.urlToImage} alt="Imagem da notícia" />}
-            <h3>{article.title}</h3>
-            <p>{article.description}</p>
-            <a href={article.url} target="_blank" rel="noopener noreferrer">Leia mais</a>
-          </li>
-        ))}
+      <div className={styles.container}>
+  {news.map((article, index) => (
+    <li key={index} className={styles.card}>
+      {article.urlToImage && <img src={article.urlToImage} alt="Imagem da notícia" />}
+      <h3>{article.title}</h3>
+      <p>{article.description}</p>
+      <a href={article.url} target="_blank" rel="noopener noreferrer">Leia mais</a>
+    </li>
+  ))}
+</div>
       </ul>
     </div>
   );
--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -6,6 +6,7 @@ const About = () => {
     return(
     <div>
         <h1 className={styles}>About</h1>
+        <p>Esse blog foi feito com React e Firebase</p>
     </div>)}
 
 export default About
